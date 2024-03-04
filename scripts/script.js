e95207b7554fd32f3526bfb5f6cfe8e2bea2eebe
Marnn9 <maren.narvik@gmail.com>
2024-03-04 08:39:00 +0100

    added new speitesheet, and position object

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,27 @@
 "use strict";
+
+const SheetData = {
+  Background: { x: 1392, y: 0, width: 1187, height: 771, count: 1 },
+  brickPurple: { x: 0, y: 717, width: 140, height: 41, count: 3 },
+  brickRed: { x: 0, y: 778, width: 140, height: 41, count: 3 },
+  brickYellow: { x: 0, y: 843, width: 140, height: 41, count: 3 },
+  brickBlue: { x: 0, y: 906, width: 140, height: 41, count: 3 },
+
+  buttons: { x: 0, y: 146, width: 55, height: 55, count: 4 },
+  startBtn: { x: 0, y: 76, width: 186, height: 56, count: 5 },
+
+  smallBar: { x: 0, y: 285, width: 158, height: 17, count: 1 },
+  largeBar: { x: 159, y: 285, width: 226, height: 17, count: 1 },
+
+  ball: { x: 79, y: 220, width: 30, height: 30, count: 1 },
+
+  numberSmall: { x: 0, y: 1329, width: 23, height: 29, count: 10 },
+  numberLarge: { x: 0, y: 1373, width: 50, height: 60, count: 10 },
+  menu: { x: 1572, y: 780, width: 830, height: 671, count: 1 },
+  noOfCrushedBricks: { x: 0, y: 217, width: 35, height: 34, count: 1 },
+
+};
+
 let cvs,
   ctx,
   speed,
@@ -98,7 +121,7 @@ function TCreateBall() {
   vector = calculateNormalizedVector(this.center, { x: 300, y: 200 });
   speed = calculateSpeedVector(vector, constantSpeed);
 
-  this.moveBall = function () {};
+  this.moveBall = function () { };
 
   this.draw = function () {
     ctx.fillStyle = color.toString();
